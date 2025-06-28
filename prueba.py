7ae166834b8e1d6c9c9c7ce7cefc0e68da7bd939
joelalejandroad <joelalejandroaranadiaz@gmail.com>
2025-06-28 14:33:13 -0600

    Modificaciones

--- prueba.py
+++ prueba.py
@@ -1,3 +1,7 @@
 print("Hello, World!")
 print ("This is a test of the print function.")
+list = [1, 2, 3, 4, 5]
+for i in list:
+    print(i)
+print("This is a test of the print function with a list.")
 
