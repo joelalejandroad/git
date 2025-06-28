69df4831a4d0b7f1ec7b3db24d9125e212c81b2d
joelalejandroad <joelalejandroaranadiaz@gmail.com>
2025-06-28 15:15:34 -0600

    Creación del login

--- prueba 2.py
+++ prueba 2.py
@@ -1,1 +1,4 @@
 print ("lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+for i in range(10):
+    print(i)
+print("This is a test of the print function with a loop.")
