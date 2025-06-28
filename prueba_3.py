7f72d6691908df5f588b15acd756092319b56fcc
joelalejandroad <joelalejandroaranadiaz@gmail.com>
2025-06-28 15:00:32 -0600

    Creación de prueba_3

---
+++ prueba_3.py
@@ -0,0 +1,6 @@
+print("Hello, World!")
+print("This is a test of the print function.")
+list = [1, 2, 3, 4, 5]
+for i in list:
+    print(i)
+print("This is a test of the print function with a list.")
